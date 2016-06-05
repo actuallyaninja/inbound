c91361464b8af9c811a46d0393fad9a19e410687
actuallyaninja <davidpbrooks@gmail.com>
2016-06-05 13:55:27 -0500

    1.23 release - more colors, tweaks -- DATE BUG

--- src/main.c
+++ src/main.c
@@ -427,7 +427,7 @@ static void update_time() {
   currentMonthDay = tick_time->tm_mday;
   
   //testing:
-  //currentMonthDay = 12;
+  currentMonthDay = 12;
   
   if(currentMonthDay > 9){
     strftime(month_and_weekday_buffer,sizeof(month_and_weekday_buffer),"%b    %n%A",tick_time);
